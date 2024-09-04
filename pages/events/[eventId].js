5fc75c4e8dc4fd2ecc6d59faff7cea26704e254e
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-09-04 17:54:41 +0530

    Add dynamic eventId component and its styling

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,5 +1,31 @@
+import { Fragment } from "react";
+import { getEventById } from "../../dummy-data";
+import { useRouter } from "next/router";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
 const EventDetailPage = () => {
-  return <div>Event Detail</div>;
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+  if (!event) {
+    return <p>No event found !</p>;
+  }
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 };
 
 export default EventDetailPage;
